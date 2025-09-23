5605c238a3b60677ab2140262b280a75b102ed44
Fabricio Rufino Gomes <fabricioruf@gmail.com>
2025-09-23 09:28:37 -0300

    .
    .

---
+++ ProjetoSistemaPagamento/FinanRP/src/main/java/com/example/FinanRP/FinanRpApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinanRP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanRpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanRpApplication.class, args);
+	}
+
+}
